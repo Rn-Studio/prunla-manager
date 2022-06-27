00dfb393c6dbfc7b7226e3ceadfbbce96c8e4cdb
pai233 <gugugupai233@gmail.com>
2022-06-27 14:46:33 +0800

    feat: 启动自检database下的git是否初始化

--- src/init.js
+++ src/init.js
@@ -6,14 +6,26 @@ const figlet = require('figlet')
 const chalk = require('chalk')
 const path = require('path')
 const fs = require('fs-extra')
+const spawn = require('child_process').spawnSync
+
+const storesPath = `${path.resolve(__dirname,'..')}/stores/`
 
 const checkDir = () => { // 检查目录是否存在
-    const storesPath = `${path.resolve(__dirname,'..')}/stores/`
     filesManager.mkdir(storesPath)
     const softsPath = `${storesPath}/softs/`
     filesManager.mkdir(softsPath)
     const unpublishedPath = `${storesPath}/unpublished/`
     filesManager.mkdir(unpublishedPath)
+}
+
+const checkRepo = () => {
+    if(!fs.pathExistsSync(`${storesPath}/.git/`)) {
+        spawn("git",[
+            "init"
+        ],{
+            cwd: storesPath
+        })
+    }
 }
 
 const init = () => {
@@ -29,6 +41,7 @@ const init = () => {
     )// 输出程序名、版本号
 
     checkDir()
+    checkRepo()
 }
 
 module.exports = {
