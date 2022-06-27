46bf9e6a4e89c9aa1c24ca792142d540937c52c2
pai233 <gugugupai233@gmail.com>
2022-06-27 14:04:38 +0800

    feat: 添加注释，增加可读性

--- src/remove.js
+++ src/remove.js
@@ -3,17 +3,22 @@ const fs = require('fs-extra')
 const path = require('path')
 
 const remove = (pkgName) => {
+    // 移动文件至Unpublished文件夹
     fs.copySync(`${path.resolve(__dirname,'..')}/stores/softs/${pkgName}`,`${path.resolve(__dirname,'..')}/stores/unpublished/${pkgName}`)
     fs.removeSync(`${path.resolve(__dirname,'..')}/stores/softs/${pkgName}`)
+
+    // 删除分类内记录
     const sort = JSON.parse(fs.readFileSync(`${path.resolve(__dirname,'..')}/stores/unpublished/${pkgName}/info.json`).toString()).sort
     let all = JSON.parse(fs.readFileSync(`${path.resolve(__dirname,'..')}/stores/all.json`).toString())
     let index = all.list.findIndex(e=>{return e.pkgName==pkgName})
     all.list.splice(index,1)
     fs.writeFileSync(`${path.resolve(__dirname,'..')}/stores/all.json`,JSON.stringify(all))
+
     let sortInfo = JSON.parse(fs.readFileSync(`${path.resolve(__dirname,'..')}/stores/${sort}.json`).toString())
     index = sortInfo.list.findIndex(e=>{return e.pkgName==pkgName})
     sortInfo.list.splice(index,1)
     fs.writeFileSync(`${path.resolve(__dirname,'..')}/stores/${sort}.json`,JSON.stringify(sortInfo))
+
     console.log(chalk.green("INFO")+" Completed.")
 }
 
